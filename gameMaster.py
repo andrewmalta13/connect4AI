3591e08196930767021ea96fdb570e7bca3ca4f2
Andrew Malta <andrew.malta@yale.edu>
2015-11-25 14:42:57 -0800

    deleted folder, fixed a IOB bug

--- gameMaster.py
+++ gameMaster.py
@@ -4,15 +4,16 @@ gameOver = False
 board = gameState.gameState()
 
 if __name__ == "__main__":
-	while(not gameOver):
-		nextMove = -1
-		retVal = -1
-		while(nextMove < 0):
-			print board
-			nextMove = input("Player " + str(board.turn) + ": Which column would you like to play a token in? ")
-			retVal = board.insert(nextMove)
-		if retVal != 0:
-			gameOver = retVal
-			break
-	print board
-	print "Game Over! Code: " + str(gameOver)
+  while(not gameOver):
+    nextMove = None
+    retVal = 0
+    while(nextMove < 0):
+      print board
+      nextMove = input("Player " + str(board.turn) + ": Which column would you like to play a token in? ")
+      if ((nextMove < len(board.heights)) and (nextMove >= 0)):
+        retVal = board.insert(nextMove)
+    if retVal != 0:
+      gameOver = retVal
+      break
+  print board
+  print "Game Over! Code: " + str(gameOver)
