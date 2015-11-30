1ced009f394482942d4516164e77927d7103ba7a
Ted <ted.tuckman@gmail.com>
2015-11-30 15:20:08 -0500

    Added command line args

--- gameMaster.py
+++ gameMaster.py
@@ -1,8 +1,30 @@
 import gameTree
 import aiplayer
-gameOver = False
+import sys
+#from stack OverFlow
+def validateInputs(val):
+    try: 
+        int(val)
+        return True
+    except ValueError:
+        return False
 
-ourGame = gameTree.gameTree()
+# setup with command arguments
+gameOver = False
+numArgs = len(sys.argv)
+ourGame = 0
+#specified height,width
+if numArgs == 3:
+    if not validateInputs(sys.argv[1]) or not sys.argv[2]:
+        print "Invalid argument"
+        sys.exit(1)
+    ourGame = gameTree.gameTree(sys.argv[1],sys.argv[2])
+#no arguments
+elif numArgs == 1:
+    ourGame = gameTree.gameTree()
+else:
+    print "Invalid number of arguments"
+    sys.exit(1)
 aiPlayer = aiplayer.AiPlayer(2, ourGame)
 
 if __name__ == "__main__":
@@ -24,3 +46,4 @@ if __name__ == "__main__":
             break
     print ourGame.tree[ourGame.position]
     print "Game Over! Code: " + str(gameOver)
+
