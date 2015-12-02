ef5373a8fc64b32428c93efca3a0c848261553ea
Andrew Malta <andrew.malta@yale.edu>
2015-12-02 00:28:27 -0500

    fixed list comprehension

--- aiplayer.py
+++ aiplayer.py
@@ -22,7 +22,6 @@ def miniMax(gameState, depth, minim, maxim, areWeMaximizing):
         for i in range(gameState.boardWidth):
             if gameState.heights[i] < gameState.boardHeight:
                 newState = gameState.getState(i)
-                # print newState
                 temp = miniMax(newState, depth - 1, minim, result, (not areWeMaximizing))
                 if temp < result:
                     result = temp
@@ -31,25 +30,24 @@ def miniMax(gameState, depth, minim, maxim, areWeMaximizing):
         return result
 
 def breakTies(gameState, moveVals):
-  maxVal = max(moveVals)
-  possibleChoices = []
-  for i in range(len(moveVals)):
-    if (moveVals[i] == maxVal) and (gameState.heights[i] < gameState.boardHeight):
-        #if this move results in a win. Take it
-        newState = gameState.getState(i)
-        if newState.heuristicValue == gameHeuristic.WIN_VALUE:
-             return i
+    maxVal = max(moveVals)
+    possibleChoices = []
+    for i in range(len(moveVals)):
+        if (moveVals[i] == maxVal) and (gameState.heights[i] < gameState.boardHeight):
+            #if this move results in a win. Take it
+            newState = gameState.getState(i)
+            if newState.heuristicValue == gameHeuristic.WIN_VALUE:
+                return i
 
-        for j in range(len(moveVals)):
-            if (newState.heights[j] < newState.boardHeight
-              and abs(newState.getState(j).heuristicValue) != gameHeuristic.WIN_VALUE):
-                possibleChoices.append(i)
-
-    
-
+            for j in range(len(moveVals)):
+                if (newState.heights[j] < newState.boardHeight 
+                    and abs(newState.getState(j).heuristicValue) != gameHeuristic.WIN_VALUE):
+                    possibleChoices.append(i)
   
+    if possibleChoices == []:
+        possibleChoices = [i for i in range(len(moveVals)) if moveVals[i] != float("-inf")]
 
-  return possibleChoices[int(random.random() * len(possibleChoices))]
+    return possibleChoices[int(random.random() * len(possibleChoices))]
 
 
 class AiPlayer(object):
